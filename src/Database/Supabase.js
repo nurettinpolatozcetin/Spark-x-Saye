308bdee5e43299db45727313144d79667f167322
npolatozcetin <ozcetinnurettinpolat@gmail.com>
2026-04-26 14:38:40 +0300

    fix supabase env

--- src/Database/Supabase.js
+++ src/Database/Supabase.js
@@ -1,7 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+const supabaseKey =
+  import.meta.env.VITE_SUPABASE_ANON_KEY || import.meta.env.VITE_SUPABASE_KEY;
 
 console.log("SUPABASE URL:", supabaseUrl);
 console.log("SUPABASE KEY:", supabaseKey);
